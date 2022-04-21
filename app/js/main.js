5de64436f9324b1f9373c4465b66a3da3af546d0
SvayOff <marsiul2001@gmail.com>
2022-04-21 12:59:34 +0300

    added slider and change drop-down

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,9 @@
 $(function () {
-});
+  $('.slider__inner').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: '<button class="slick-arrow slick-next"><span class="lnr lnr-chevron-right"></span></button>',
+    prevArrow: '<button class="slick-arrow slick-prev"><span class="lnr lnr-chevron-left"></span></button>',
+  })
+})
