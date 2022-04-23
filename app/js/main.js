9142c64e57764d66748a1aa1108c0d826adc93f9
SvayOff <marsiul2001@gmail.com>
2022-04-23 15:42:10 +0300

    added specialists

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,16 @@ $(function () {
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
     nextArrow: '<button class="slick-arrow slick-next"><span class="lnr lnr-chevron-right"></span></button>',
     prevArrow: '<button class="slick-arrow slick-prev"><span class="lnr lnr-chevron-left"></span></button>',
-  })
+  }),
+    $('.specialists__slider').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: false,
+      dots: true,
+    })
 })
